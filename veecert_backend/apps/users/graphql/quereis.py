ffd934d7ebeaf307ec0f2aeb7a99ed40ffed346b
Ezekiel Victor <codeepoch@gmail.com>
2024-06-12 11:58:14 +0100

    add user client query

--- veecert_backend/apps/users/graphql/quereis.py
+++ veecert_backend/apps/users/graphql/quereis.py
@@ -1,7 +1,7 @@
 from typing import List
 import strawberry
 
-from veecert_backend.apps.users.graphql.types.outputs import PackageType
+from veecert_backend.apps.users.graphql.types.outputs import ClientType, PackageType
 
 
 @strawberry.type
@@ -12,3 +12,10 @@ class UserQueries:
 
         packages = await Package.manager.all()
         return list(map(lambda item: PackageType.from_model(item), packages))
+
+    @strawberry.field
+    async def user_client(self, user_id: int) -> ClientType:
+        from ..models import Client
+
+        client = await Client.manager.one_by_user_id(user_id)
+        return ClientType.from_model(client)
